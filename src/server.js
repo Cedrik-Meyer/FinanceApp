d9ca2d59be90c939725af70f50a7296af1d0d4c9
Cedrik-Meyer <cedrik.meyer02@gmail.com>
2026-04-27 14:02:04 +0200

    User management with login

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const accountRoutes = require('./routes/accounts');
 const transactionRoutes = require('./routes/transactions');
 const depotRoutes = require('./routes/depot');
+const authRoutes = require('./routes/auth');
+const authMiddleware = require('./middleware/auth');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use('/api/auth', authRoutes)
 app.use('/api/accounts', accountRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/depot', depotRoutes);
